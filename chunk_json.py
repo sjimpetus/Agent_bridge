7b169245deb2d9ff843e467134c5964ed9680b76
sjimpetus <sj001002003004@gmail.com>
2026-09-08 10:43:19 +0530

    Add files via upload

---
+++ chunk_json.py
@@ -0,0 +1,17 @@
+import json
+
+with open("chunks.json", "r") as f:
+    data = json.load(f)
+
+chunk_size = len(data)
+chunk_count = 0
+
+for i in range(0, len(data), chunk_size):
+    chunk_count += 1
+    with open(f"chunk_{chunk_count}.json", "w") as f:
+        for record in data[i:i+chunk_size]:
+            json.dump(record, f)
+            f.write("\n")
+#print(len())
+
+print("Chunked files created.")
