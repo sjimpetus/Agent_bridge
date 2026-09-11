6e2b3ac342581be5d3c43225fa97ddcb717bb9eb
sjimpetus <sj001002003004@gmail.com>
2026-09-11 15:02:06 +0530

    new changes

--- chunk_json.py
+++ chunk_json.py
@@ -1,17 +1,1 @@
-import json
-
-with open("chunks.json", "r") as f:
-    data = json.load(f)
-
-chunk_size_len = len(data)
-token = 0
-
-for i in range(0, len(data), chunk_size):
-    chunk_count += 1
-    with open(f"chunk_{chunk_count}.json", "w") as f:
-        for record in data[i:i+chunk_size]:
-            json.dump(record, f)
-            f.write("\n")
-#print(len())
-
-print("Chunked files created.")
+data1 = json.load(f)
